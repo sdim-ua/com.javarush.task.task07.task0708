e80488433363148e48c3bb02d0d44f073ed306bd
sdim-ua <ingersol@yandex.ua>
2019-02-07 23:15:07 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Самая длинная строка
+*/
+
+public class Solution {
+    private static List<String> strings;
+
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        strings = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+         int max = 0; 
+        
+         for (int i = 0; i < 5; i++){
+            String s = reader.readLine();
+            strings.add(i, s); 
+            
+            
+        if (strings.get(i).length() > max) {
+            max = strings.get(i).length();    
+                    }
+              
+                }
+              for (String string : strings) {
+            if (max == string.length()) {
+                System.out.println(string);
+                }  
+                    
+            }
+            
+        }
+
+    }
+
